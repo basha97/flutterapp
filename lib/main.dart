d3def093eb02109d686c9ee504226b7555b51187
basha97 <bashaadil2727@gmail.com>
2019-08-05 15:55:30 +0530

    Scaffolding a flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Center(
-  child: Text('Hello World'),
-)));
-
-
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('Basha'),
+      backgroundColor: Colors.redAccent,
+    ),
+    backgroundColor: Colors.blueGrey[900],
+    body: Center(
+      child: Image(
+        image: NetworkImage('https://ezfleets.com/backend/images/logo.png'),
+      ),
+    ),
+  ),
+));
